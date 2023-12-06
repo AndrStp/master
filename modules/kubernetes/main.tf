f516c4408a33f2346b6d9bdb915970abec724b0b
Andrey Stepanenko <ast.university@hotmail.com>
2023-12-06 21:05:44 +0200

    feat: add ServiceAccount to ns to pull secrets from private repo

--- modules/kubernetes/main.tf
+++ modules/kubernetes/main.tf
@@ -12,22 +12,6 @@ resource "kubernetes_namespace_v1" "master" {
   }
 }
 
-resource "kubernetes_config_map_v1" "config" {
-  metadata {
-    name      = var.configmap_name
-    namespace = kubernetes_namespace_v1.master.metadata[0].name
-
-    labels = {
-      env = "master"
-    }
-  }
-
-  data = {
-    NAME     = var.config_env_name
-    LOCATION = var.config_env_location
-  }
-}
-
 resource "kubernetes_secret_v1" "docker_cfg" {
   metadata {
     name = "docker-cfg"
@@ -51,5 +35,71 @@ resource "kubernetes_secret_v1" "docker_cfg" {
         }
       }
     })
+  }
+}
+
+resource "kubernetes_service_account_v1" "master" {
+  metadata {
+    name      = var.master_namespace_name
+    namespace = var.master_namespace_name
+
+    labels = {
+      env = "master"
+    }
+  }
+  image_pull_secret {
+    name = "docker-cfg"
+  }
+}
+
+resource "kubernetes_cluster_role_v1" "master" {
+  metadata {
+    name = var.master_namespace_name
+
+    labels = {
+      env = "master"
+    }
+  }
+
+  rule {
+    api_groups = ["*"]
+    resources  = ["*"]
+    verbs      = ["*"]
+  }
+}
+
+resource "kubernetes_cluster_role_binding" "master" {
+  metadata {
+    name = var.master_namespace_name
+
+    labels = {
+      "app.kubernetes.io/managed-by" = "Terraform"
+    }
+  }
+  role_ref {
+    api_group = "rbac.authorization.k8s.io"
+    kind      = "ClusterRole"
+    name      = var.master_namespace_name
+  }
+  subject {
+    kind      = "ServiceAccount"
+    name      = var.master_namespace_name
+    namespace = var.master_namespace_name
+  }
+}
+
+resource "kubernetes_config_map_v1" "config" {
+  metadata {
+    name      = var.configmap_name
+    namespace = kubernetes_namespace_v1.master.metadata[0].name
+
+    labels = {
+      env = "master"
+    }
+  }
+
+  data = {
+    NAME     = var.config_env_name
+    LOCATION = var.config_env_location
   }
 }
